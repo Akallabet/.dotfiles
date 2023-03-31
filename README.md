a759455d2b031ec3725a0c60b87ff7eca6458761
Graziano Statello <grazianos86@gmail.com>
2023-03-31 19:33:44 +0100

    use stow to manage dotfiles

---
+++ README.md
@@ -0,0 +1,38 @@
+# Dotfiles
+
+## Install oh-my-zsh
+```
+git submodule update --init
+```
+
+## Install Homebrew
+```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+```
+
+## Ansible
+
+Make sure phython3 is installed, then
+
+```
+curl https://bootstrap.pypa.io/get-pip.py | python3
+```
+
+## Install nodenv
+```
+brev install nodenv
+```
+
+## Neovim
+
+You need to instal `nvim` and `ripgrep`
+
+## Web fonts
+
+```
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
+```
+
+Enable web fonts in iterm
+`Settings -> Profiles -> Text -> Non-ASCII Font`
---
+++ init.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Install Homebrew if not exists
+if ! command -v brew &> /dev/null
+then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Install stow if not exists
+if ! command -v stow &> /dev/null
+then
+	brew install stow
+fi
+
+stow nvim
+stow zsh
+
+# Install oh-my-zsh
+git submodule update --init
+
+# Install nvim if not exists
+if ! command -v nvim &> /dev/null
+then
+	brew install neovim
+fi
+
+# Install ripgrep if not exists
+if ! command -v rg &> /dev/null
+then
+	brew install ripgrep
+fi
+
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
+
+# Install nodenv if not exists
+if ! command -v nodenv &> /dev/null
+then
+	brew install nodenv
+fi
