35daf441a228fe39edca3d18c2d043be5155cfe1
Graziano Statello <graziano.statello@external.mckinsey.com>
2023-05-26 16:49:34 +0100

    Remove duplicate modeline config

--- nvim/.config/nvim/lua/post-setup.lua
+++ nvim/.config/nvim/lua/post-setup.lua
@@ -268,8 +268,5 @@ return {
     -- require('copilot').setup({
     --   enable = true,
     -- })
-
-    -- The line beneath this is called `modeline`. See `:help modeline`
-    -- vim: ts=2 sts=2 sw=2 et
   end
 }
