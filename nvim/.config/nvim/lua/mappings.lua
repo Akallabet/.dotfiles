050739cd068e64767a1aab4cd4fa656eb60cdd7e
Graziano Statello <graziano.statello@external.mckinsey.com>
2023-05-26 16:19:19 +0100

    Move plugins in own folder

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -24,8 +24,11 @@ return {
     vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
     -- Telescope
-    vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-    vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>?', function() require('telescope.builtin').oldfiles() end,
+      { desc = '[?] Find recently opened files' })
+    vim.keymap.set('n', '<leader><space>', function()
+      require('telescope.builtin').buffers()
+    end, { desc = '[ ] Find existing buffers' })
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
       require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -39,10 +42,13 @@ return {
         find_command = { 'rg', '--files', '--hidden', '-g', '!.git' },
       })
     end, { desc = '[F]ind [F]iles' })
-    vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
-    vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
-    vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
-    vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
+    vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags() end, { desc = '[F]ind [H]elp' })
+    vim.keymap.set('n', '<leader>fw', function() require('telescope.builtin').grep_string() end,
+      { desc = '[F]ind current [W]ord' })
+    vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end,
+      { desc = '[F]ind by [G]rep' })
+    vim.keymap.set('n', '<leader>fd', function() require('telescope.builtin').diagnostics() end,
+      { desc = '[F]ind [D]iagnostics' })
 
     -- Fugitive
     vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = "Git status" })
@@ -50,13 +56,8 @@ return {
     -- Neotree
     vim.keymap.set({ 'n', 'v' }, '<leader>n', ':Neotree toggle<CR>', { silent = true })
 
-    -- Bufferline
-    require('bufferline').setup({})
-    vim.keymap.set({ 'n', 'v' }, 'H', ':BufferLineCyclePrev<CR>', { silent = true })
-    vim.keymap.set({ 'n', 'v' }, 'L', ':BufferLineCycleNext<CR>', { silent = true })
-
-    -- Bufdelete
-    vim.keymap.set({ 'n', 'v' }, '<leader>bc', ':Bwipeout<CR>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, 'H', ':BufferPrevious<CR>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, 'L', ':BufferNext<CR>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<leader>bc', ':BufferClose<CR>', { silent = true })
   end
 }
-
