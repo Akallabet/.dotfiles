eac3cca52f3c947d363fde64b8123a06cf9342c0
Graziano Statello <grazianos86@gmail.com>
2023-10-09 10:29:45 +0200

    Update indent-blankline config

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -34,9 +34,9 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = { char = "|" },
     },
   },
   -- "gc" to comment visual regions/lines
