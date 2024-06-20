5864b5ea4d8c68b0460e63b50e8fc35b5a7a7eba
Graziano Statello <grazianos86@gmail.com>
2024-06-20 18:46:25 +0200

    add codeium

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -1,9 +1,23 @@
 return {
-  'github/copilot.vim',
-  config = function()
-    -- Copilot
-    -- require('copilot').setup({
-    --   enable = true,
-    -- })
-  end
+  {
+    'github/copilot.vim',
+    config = function()
+      -- Copilot
+      -- require('copilot').setup({
+      --   enable = true,
+      -- })
+    end
+  },
+  {
+    'Exafunction/codeium.vim',
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
+  }
 }
